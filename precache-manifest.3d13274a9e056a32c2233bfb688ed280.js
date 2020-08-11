b6e0da2156c9e6493d8a5f2b5fea1cddbb656de2
unknown <kumarharsh241998@gmail.com>
2020-08-11 20:52:28 +0530

    Updates

---
+++ precache-manifest.3d13274a9e056a32c2233bfb688ed280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b407a9564ed88806d0c1e66a9136e1ab",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "776171bbd846d877a4ec",
+    "url": "/meme-generator/static/css/main.e21d1cfa.chunk.css"
+  },
+  {
+    "revision": "07d433d765748c864c76",
+    "url": "/meme-generator/static/js/2.34cc477c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.34cc477c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776171bbd846d877a4ec",
+    "url": "/meme-generator/static/js/main.416cc13d.chunk.js"
+  },
+  {
+    "revision": "2a80e2ff224d17e65492",
+    "url": "/meme-generator/static/js/runtime-main.6641b1f8.js"
+  }
+]);
